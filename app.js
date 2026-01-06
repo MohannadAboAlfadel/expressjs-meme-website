7826a79c3b6069b3ec0557d794496b5e2c6aae81
memo4214 <133568307+memo4214@users.noreply.github.com>
2026-01-06 22:15:51 +0300

    show all memes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.set("view engine", "ejs")
 app.set("views", "views")
 
-const memes = [
+let memes = [
     {
         name: "Memes that make us smile",
         imgURL: "https://exse.eyewated.com/pict/c38cc4d7ef9a30de-1024x683.jpg",
@@ -18,11 +18,24 @@ const memes = [
         imgURL: "https://aprogrammerlife.com/images/pictuers/programmers_looking_at_programming_memes.jpg",
         price: "$10.00"
     }
-]
+];
+
 app.get("/", (req, res) => {
     res.render("index",{memes})
 })
+app.get("/add-meme", (req, res) => {
+    res.render("add-meme")
+})
+app.post("/memes", (req, res) => {
+    const newMeme = {
+        name: req.body.name,
+        price: req.body.price,
+        imgURL: req.body.imgURL
+    };
 
+    memes.push(newMeme);
+    res.redirect("/");
+});
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server running: http://localhost:${PORT}`))
